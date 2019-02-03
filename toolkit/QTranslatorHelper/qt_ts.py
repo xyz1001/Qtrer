ca9e492f0c828fcfec1dcb88d76216411d677885
leytou <hi_litao@163.com>
2019-02-03 12:31:02 +0800

    [modify] 当翻译为空时使用英文翻译替代

--- toolkit/QTranslatorHelper/qt_ts.py
+++ toolkit/QTranslatorHelper/qt_ts.py
@@ -6,61 +6,61 @@ import logging
 
 
 class QtTs(object):
-    def __init__(self, ts_path):
+    def __init__(self, ts_path, tr_table):
         self.ts_path = ts_path
         self.xml_tree = ElementTree.parse(ts_path)
-        self.target_locale_name = ""
+        self.tr_table = tr_table
 
-    def set_target_language(self, locale_name):
-        self.target_locale_name = locale_name
-        root = self.xml_tree.getroot()
-        root.attrib["language"] = self.target_locale_name
-
-    def __get_source_strings(self, translation_table):
-        if "source" in translation_table:
-            return translation_table["source"]
+    def __get_tr_list(self, locale_name):
+        if locale_name in self.tr_table:
+            return self.tr_table[locale_name]
         else:
-            logging.critical("No EN language translation found")
-            exit(-1)
+            return []
 
-    def __get_target_strings(self, translation_table):
-        logging.info("target locale name: %s" %
-                     (self.target_locale_name))
-        if self.target_locale_name in translation_table:
-            logging.info("Found transltion %s" % self.target_locale_name)
-            return translation_table[self.target_locale_name]
+    def __get_target_tr_list(self, locale_name):
+        logging.info("target locale name: %s" %locale_name)
+        tr_list = self.__get_tr_list(locale_name)
+        if tr_list:
+            return tr_list
 
-        language_name = self.target_locale_name[0:2]
-        for key in translation_table.keys():
+        language_name = locale_name[0:2]
+        for key in self.tr_table.keys():
             if key.startswith(language_name):
                 logging.info("Found transltion %s" % key)
-                return translation_table[key]
+                return self.tr_table[key]
 
         logging.error("Fail to translate to %s",
-                      self.target_locale_name)
+                      locale_name)
         raise KeyError("%s: Target translation NOT found" %
-                       self.target_locale_name)
+                       locale_name)
 
-    def __get_translation(self, source_string, source_strings, target_strings):
+    def __get_tr(self, source_string, source_tr_list, target_tr_list):
         try:
-            index = source_strings.index(source_string)
-            return target_strings[index]
+            index = source_tr_list.index(source_string)
+            return target_tr_list[index]
         except (ValueError, IndexError):
             logging.error("no source string '%s' found" % source_string)
 
-    def translate(self, translation_table):
+    def tr(self, locale_name):
         root = self.xml_tree.getroot()
-        source_strings = self.__get_source_strings(translation_table)
-        target_strings = self.__get_target_strings(translation_table)
+        source_tr_list = self.__get_tr_list("source")
+        if not source_tr_list:
+             logging.error("no source list found")
+             exit(-1)
+        target_tr_list = self.__get_target_tr_list(locale_name)
+        en_tr_list = self.__get_target_tr_list("en")
         for context in root:
             for message in context:
                 if message.tag != "message":
                     continue
                 source = message.find("source")
-                target_string = self.__get_translation(
-                    source.text, source_strings, target_strings)
+                target_string = self.__get_tr(
+                    source.text, source_tr_list, target_tr_list)
                 if target_string is None:
                     continue
+                if target_string.strip() == "":                 #若翻译词条为空，使用英文翻译
+                    target_string = self.__get_tr(
+                    source.text, source_tr_list, en_tr_list)
 
                 translation = message.find("translation")
                 translation.attrib.clear()
